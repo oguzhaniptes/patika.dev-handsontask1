a174b2a01c0f71e8244a16c8569be783f3519049
oguzhaniptes <oguzhaniptes02@gmail.com>
2023-01-20 16:01:12 +0300

    todo hw is finished

--- to-do.sol
+++ to-do.sol
@@ -2,5 +2,35 @@
 pragma solidity ^0.8.7;
 
 contract ToDo{
-    
+    // todo's related data
+    struct Todo{
+        uint time;
+        string text;
+        bool complated;
+    }
+
+    Todo[] public todos;
+    // This function can create, changed and get todo's
+    function create(string calldata _text) external {
+        todos.push(Todo({
+            time: block.timestamp,
+            text: _text,
+            complated: false
+        }));
+    }
+
+    function update(uint _index, string calldata _text) external {
+        todos[_index].text = _text;
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+
+    }
+    function get(uint _index) external view returns(string memory, bool) {
+        Todo memory todo = todos[_index];
+        return (todo.text, todo.complated);
+    }
+    function complatedTodo(uint _index) external {
+        todos[_index].complated = !todos[_index].complated;
+    }
+
 }
